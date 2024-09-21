2114937f13eca96f55ed75d2bc466bb51ec429d5
rohan37-pro <rohan2002maji@gmail.com>
2024-09-21 11:14:05 +0530

    problem 386 debugging required

--- lexicographical-numbers386/leetcode386.py
+++ lexicographical-numbers386/leetcode386.py
@@ -1,3 +1,21 @@
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        pass
+    def lexicalOrder(self, n):
+        lex = 1
+        # level = 1
+        lexical = []
+        for i in range(n):
+            if lex<= n:
+                lexical.append(lex)
+                if lex*10 <= n:
+                    lex *= 10
+                    # level+=1
+                while (lex+1)//10 > lex//10: 
+                    lex = lex//10
+                    # level-=1
+                lex+=1
+
+            else:
+                while (lex+1)//10 > lex//10: 
+                    lex = lex//10
+                lex +=1
+        return lexical
