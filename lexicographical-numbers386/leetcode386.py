8c1edf21f606dcc5cc713bf005f91eb23bf46ade
rohan37-pro <rohan2002maji@gmail.com>
2024-09-21 11:33:33 +0530

    problem 386 testing successfull

--- lexicographical-numbers386/leetcode386.py
+++ lexicographical-numbers386/leetcode386.py
@@ -1,21 +1,23 @@
 class Solution:
     def lexicalOrder(self, n):
         lex = 1
-        # level = 1
+        i = 0
         lexical = []
-        for i in range(n):
+        while i < n:
             if lex<= n:
                 lexical.append(lex)
+                i+=1
                 if lex*10 <= n:
                     lex *= 10
-                    # level+=1
-                while (lex+1)//10 > lex//10: 
-                    lex = lex//10
-                    # level-=1
-                lex+=1
-
+                else:
+                    lex+=1
+                    while (lex)//10 > (lex-1)//10: 
+                        lex = lex//10
             else:
                 while (lex+1)//10 > lex//10: 
                     lex = lex//10
                 lex +=1
         return lexical
+
+obj = Solution()
+print(obj.lexicalOrder(136))
