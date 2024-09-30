a10677211d705d42812293fb513d633588a9e2b5
rohan37-pro <rohan2002maji@gmail.com>
2024-09-30 22:44:00 +0530

    a stack with increament operation was successfully designed in python, programme runtime 94sec.

---
+++ design-a-stack-with-increment-operation1381/leet1381.py
@@ -0,0 +1,28 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack)< self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack)==0:
+            return -1
+        return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        n = len(self.stack)
+        if k>n :
+            k = n
+        for i in range(k):
+            self.stack[i] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
